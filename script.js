219533e9458ab0bd5395f326c6a66aeec807785e
Adi <adityachimote@gmail.com>
2024-03-21 07:45:50 +0530

    "fixed some bugs and  adjust some content"

--- script.js
+++ script.js
@@ -7,3 +7,24 @@ list.addEventListener('click', () => {
     header.classList.toggle("h-nav");
 });
 
+document.addEventListener('DOMContentLoaded', function () {
+    var Books = document.getElementById('vboook_load');
+    aboutTab.addEventListener('click', function (event) {
+      event.preventDefault(); // Prevent the default link behavior
+      
+      fetch('about.html')
+        .then(response => {
+          if (!response.ok) {
+            throw new Error('Page not found');
+          }
+          return response.text();
+        })
+        .then(data => {
+          document.getElementById('content').innerHTML = data;
+        })
+        .catch(error => {
+          console.error('Error loading page:', error);
+        });
+    });
+  });
+  
